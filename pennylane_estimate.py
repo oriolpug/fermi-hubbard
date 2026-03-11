c52ed8415cd5fd5dbb518109aeb8fe0507b5192a
uri <oriol@qoroquantum.net>
2026-03-11 17:57:30 +0100

    grouped observables

--- pennylane_estimate.py
+++ pennylane_estimate.py
@@ -77,6 +77,7 @@ def expect_maestro(config: dict = None, chi: int = 64) -> None:
     return elapsed
 if __name__ == "__main__":
     try:
+        print(sys.argv)
         if len(sys.argv) == 0 or (len(sys.argv) == 1 and sys.argv[0] in ["--gpu", "--pennylane"]) or (len(sys.argv) == 2 and "--pennylane" in sys.argv):
             print(f"Benchmarking Pennylane")
             expect_pennylane(chi=64)
